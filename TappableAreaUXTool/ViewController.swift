8054f59a21e5e5480f861e2bb2db5022babddeed
Abhishek kapoor <theabhieye@gmail.com>
2026-01-04 13:26:04 +0530

    Initial Commit

---
+++ TappableAreaUXTool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TappableAreaUXTool
+//
+//  Created by Abhishek kapoor on 04/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
